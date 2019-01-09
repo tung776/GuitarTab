9154f54ac30154211d85421bdc09e99ce421882b
tung776 <thanhtung776@gmail.com>
2019-01-09 09:33:59 +0700

    hash password when password changed

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,18 @@
+const Promise = require('bluebird')
+const bcrypt = Promise.promisifyAll('bcrypt-nodejs')
+
+function hashPassword(user) {
+    const SALT_FACTOR = 8;
+    if (!user.changed('password')) {
+        return;
+    }
+
+    return bcrypt.genSaltAsync(SALT_FACTOR)
+        .then(salt => bcrypt.hashAsync(user.password, salt, null))
+}
+
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("User", {
+    const User = sequelize.define("User", {
         email: {
             type: DataTypes.STRING(126),
             unique: true
@@ -7,5 +20,16 @@ module.exports = (sequelize, DataTypes) => {
         password: {
             type: DataTypes.STRING(126)
         }
+    }, {
+        hooks: {
+            beforeCreate: hashPassword,
+            beforeUpdate: hashPassword,
+            beforeSave: hashPassword
+        }
     });
+
+    User.prototype.comparePassword = (password) => {
+        return bcrypt.compareAsync(password, this.password)
+    }
+    return URLSearchParams;
 };
