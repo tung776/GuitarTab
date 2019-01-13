376614f41c26598df497990f514211edb371e2bc
tung776 <thanhtung776@gmail.com>
2019-01-13 10:58:30 +0700

    ...

--- services/api.js
+++ services/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
+import {
+    state
+} from '@/store';
+import mapState from 'vuex'
 
 export default () => {
-  return axios.create({
-    baseURL: `http://localhost:3000/api`
-  });
+    console.log('state =', authen)
+    const {
+        token
+    } = [...mapState['token']]
+    return axios.create({
+        baseURL: `http://localhost:3000/api`,
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
 };
