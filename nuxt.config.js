6d35298d10538c5f021a972506e5470fa7e97afa
tung776 <thanhtung776@gmail.com>
2019-01-14 08:07:35 +0700

    middleware clear validation error

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,95 +1,113 @@
 const pkg = require("./package");
 
 module.exports = {
-  mode: "universal",
+    mode: "universal",
 
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: pkg.description }
-    ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
-      }
-    ]
-  },
-
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: "#fff" },
-
-  /*
-   ** Global CSS
-   */
-  css: ["~/assets/style/app.styl"],
-
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [
-    "@/plugins/axios.js",
-    "@/plugins/vuetify",
-    "@/plugins/user.js",
-    "@/plugins/validation.js"
-  ],
-
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://github.com/nuxt-community/axios-module#usage
-    "@nuxtjs/axios",
-    "@nuxtjs/auth"
-  ],
-  auth: {
-    strategies: {
-      local: {
-        endpoints: {
-          login: {
-            url: "login",
-            method: "post",
-            propertyName: "token"
-          },
-          logout: {
-            url: "logout",
-            method: "post"
-          },
-          user: {
-            url: "user",
-            method: "get",
-            propertyName: "user"
-          }
-        },
-        tokenRequired: true,
-        tokenType: "bearer"
-      }
-    }
-  },
-  /*
-   ** Axios module configuration
-   */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-    baseURL: "http://localhost:3000/api/"
-  },
-
-  /*
-   ** Build configuration
-   */
-  build: {
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) {}
-  }
+    head: {
+        title: pkg.name,
+        meta: [{
+                charset: "utf-8"
+            },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            },
+            {
+                hid: "description",
+                name: "description",
+                content: pkg.description
+            }
+        ],
+        link: [{
+                rel: "icon",
+                type: "image/x-icon",
+                href: "/favicon.ico"
+            },
+            {
+                rel: "stylesheet",
+                href: "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
+            }
+        ]
+    },
+
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: {
+        color: "#fff"
+    },
+
+    /*
+     ** Global CSS
+     */
+    css: ["~/assets/style/app.styl"],
+
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [
+        "@/plugins/axios.js",
+        "@/plugins/vuetify",
+        "@/plugins/user.js",
+        "@/plugins/validation.js"
+    ],
+
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://github.com/nuxt-community/axios-module#usage
+        "@nuxtjs/axios",
+        "@nuxtjs/auth"
+    ],
+    auth: {
+        strategies: {
+            local: {
+                endpoints: {
+                    login: {
+                        url: "login",
+                        method: "post",
+                        propertyName: "token"
+                    },
+                    logout: {
+                        url: "logout",
+                        method: "post"
+                    },
+                    user: {
+                        url: "user",
+                        method: "get",
+                        propertyName: "user"
+                    }
+                },
+                tokenRequired: true,
+                tokenType: "bearer"
+            }
+        }
+    },
+
+    router: {
+        middleware: [
+            'clearValidationError'
+        ]
+    },
+    /*
+     ** Axios module configuration
+     */
+    axios: {
+        // See https://github.com/nuxt-community/axios-module#options
+        baseURL: "http://localhost:3000/api/"
+    },
+
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
 };
